b2b7d3cfd45118da264352d4416fbf6088819abb
Dominique Charlebois <domo.charlebois@gmail.com>
2018-03-21 16:29:19 -0700

    Added Javascript

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Set URL and Key
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/18U_aqDsC1Vlj0_wv7l6I2tNrxbzLCSn6iBq9l9rFvr4/pubhtml';
+
+// Call tabletop fuction
+function init() {
+  Tabletop.init( { key: publicSpreadsheetUrl,
+                   callback: showInfo,
+                   simpleSheet: true } )
+}
+
+// Function to loop through data
+function showInfo(data, tabletop) {
+  for (var i = 0; i < data.length; i++) {
+    $('.post').append(
+      "<div class='article'>" +
+        "<div class='text'>" +
+          "<h1>" + data[i].title + "</h1>" +
+          "<h4> by " + data[i].author + "</h4>" +
+          "<p>" + data[i].body + "</p>" +
+          "<p class='date'> Published: "  + data[i].date + "</p>" +
+        "</div>" +
+      "</div>"
+    );
+  }
+}
+
+window.addEventListener('DOMContentLoaded', init);
